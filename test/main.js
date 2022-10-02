69c77284839f3ed0724d45870e208dd43c530981
ehmicky <ehmicky@gmail.com>
2022-10-02 19:49:12 +0200

    Fix linting

--- test/main.js
+++ test/main.js
@@ -41,7 +41,6 @@ ARGS.forEach((args) => {
 const INVALID_ARGS = [
   true,
   [undefined],
-  // eslint-disable-next-line unicorn/no-null
   [null],
   [[], true],
   [() => true],
@@ -52,7 +51,6 @@ const INVALID_ARGS = [
   ],
   [
     function getNull() {
-      // eslint-disable-next-line unicorn/no-null
       return null
     },
   ],
